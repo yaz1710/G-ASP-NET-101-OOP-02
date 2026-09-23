f82fae86b92bb989e0d866bc6b8a1848531232ec
yaz_1710 <yahyahosny1234567@gmail.com>
2026-09-23 17:21:37 +0300

    question 1

--- assignment opp2/Program.cs
+++ assignment opp2/Program.cs
@@ -1,1 +1,38 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace assignment oop2
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        #region  Question 1
+        // A - What is the difference between a class and a struct?
+
+        //Class: Reference type, supports inheritance
+    
+
+           //Struct: Value type, doesn't support inheritance
+
+    //B - Why are classes more suitable than structs for large applications?
+
+   // Classes are better for large applications because they support inheritance, encapsulation, and polymorphism
+
+    #endregion\
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+}
