73ea0ca765988421374d3c27c87bb173153f8dc4
yaz_1710 <yahyahosny1234567@gmail.com>
2026-09-23 17:28:54 +0300

    question 2

--- assignment opp2/Program.cs
+++ assignment opp2/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace assignment oop2
+﻿using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace assignment oop2
 internal class Program
 {
     static void Main(string[] args)
@@ -9,13 +12,33 @@ internal class Program
         // A - What is the difference between a class and a struct?
 
         //Class: Reference type, supports inheritance
-    
 
-           //Struct: Value type, doesn't support inheritance
 
-    //B - Why are classes more suitable than structs for large applications?
+        //Struct: Value type, doesn't support inheritance
 
-   // Classes are better for large applications because they support inheritance, encapsulation, and polymorphism
+        //B - Why are classes more suitable than structs for large applications?
+
+        // Classes are better for large applications because they support inheritance, encapsulation, and polymorphism
+
+        #endregion
+
+        #region question2 
+
+        a- Which class is the parent class?
+
+              Shipment
+        b- Which class is the child class?
+
+               ExpressShipment
+        c- What members are inherited by ExpressShipment?
+
+              TrackingCode
+        d- Why is inheritance better than duplicating the same code?
+
+             It avoids code duplication and makes the code easier to maintain.
+
+
+
 
     #endregion
 
@@ -30,9 +53,6 @@ internal class Program
 
 
 
-
-
-
-    }
+}
 }
 }
